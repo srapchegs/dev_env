5ea17ba2c6571fc5f9b0c149fac42c74b0ef12d0
sraperanosan <srapik614@gmail.com>
2025-04-25 17:52:49 +0500

    Теги для меню

---
+++ app1/product/templatetags/products_tags.py
@@ -0,0 +1,9 @@
+
+from django import template
+from product.models import Categories
+
+register = template.Library()
+
+@register.simple_tag()
+def tag_categories():
+    return Categories.objects.all()
