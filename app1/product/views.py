bce6d09a04275060b69cc7f629fe3ea233c04a39
sraperanosan <srapik614@gmail.com>
2025-04-25 13:01:22 +0500

    Добавлены шаблоны каталога и деталки

--- app1/product/views.py
+++ app1/product/views.py
@@ -6,10 +6,10 @@ def detail(request):
     context = {
         'title': "Детальная страница товара"
     }
-    return render(request, 'main/detail.html', context)
+    return render(request, 'product/detail.html', context)
 
 def catalog(request):
     context = {
         'title': "Блоки"
     }
-    return render(request, 'main/catalog.html', context)
+    return render(request, 'product/catalog.html', context)
