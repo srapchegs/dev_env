190628a7105a7c422899f9185e0fbd2ac4a99c42
sraperanosan <srapik614@gmail.com>
2025-05-11 22:14:04 +0500

    админка

--- app1/cars/admin.py
+++ app1/cars/admin.py
@@ -8,4 +8,27 @@ from cars.models import Cars, CarsBid
 class CarsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(CarsBid)
+@admin.register(CarsBid)
+class CarsBidAdmin(admin.ModelAdmin):
+    readonly_fields = ('user_email', 'user_first_name', 'user_last_name','user_otchestvo','user_phone', 'created_timestamp')
+    fields = ('user', 'user_email', 'user_first_name', 'user_last_name','user_otchestvo','user_phone', 'cars', 'bid_address', 'date_rent')
+
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = 'Email'
+
+    def user_first_name(self, obj):
+        return obj.user.first_name
+    user_first_name.short_description = 'Фамилия'
+
+    def user_last_name(self, obj):
+        return obj.user.last_name
+    user_last_name.short_description = 'Имя'
+
+    def user_otchestvo(self, obj):
+        return obj.user.otchestvo
+    user_otchestvo.short_description = 'Отчество'
+
+    def user_phone(self, obj):
+        return obj.user.phone
+    user_phone.short_description = 'Номер телефона'
